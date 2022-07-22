833a3144cb4e7c1e467703b70ed0170c83de0195
MonkeyDYoyi <jorgegermanperezblanco@gmail.com>
2022-07-22 13:50:02 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1118,6 +1118,8 @@ class main:
             #added by yoyi
             elif ((me.id == mainIds["yoyi"] or me.id == cousinIds["vivi"]) and ((mensaje.chat.id == cousinIds["vivi"]) or (mensaje.chat.id == cousinIds["sheik"]))):
                 mensaje.forward(ids["CW"])
+                app.send_message(ids["helper"], "tempbool está en: " + tempbool)
+                app.send_message(ids["helper"], "temp está en: " + int(temp))
                 if '/g_withdraw' in mensaje.text:
                     temp = mensaje.chat.id
                     tempbool = True
